738fe41dc84c8bd449466f9459857be702260f79
Lary Ace Pantonial <laryacepantonial017@gmail.com>
2025-09-05 10:30:41 +0800

    Add files via upload

---
+++ result.php
@@ -0,0 +1,10 @@
+<?php
+    echo "<pre>";
+
+    //array; key => value
+    $indexedArray = array("asdad",75,'a');
+
+    $associativeArray = array("firstname" => "CITE", "age" => 18);
+
+    print_r($_GET["firstname"]);
+?>
